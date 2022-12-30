1caf0541a69d317851ad29b6399ad5997d35e067
scaboodles <owenj.wolff2@gmail.com>
2022-12-30 12:33:20 -0800

    way to scrape data from ebooks to create training info

--- random_book.py
+++ random_book.py
@@ -5,15 +5,16 @@ import random
 
 def run():
     text, title, author, book_num= find_good_book()
-    print(title, author, book_num)
-    save_file = open(f"./books/{book_num}.txt",'w')
-    save_file.write(text)
-    save_file.close()
+    #save_file = open(f"./books/{book_num}.txt",'w')
+    #save_file.write(text)
+    #save_file.close()
+    return text, title, author, book_num
 
 def find_good_book():
     find_header = "START OF THE PROJECT GUTENBERG EBOOK.*"
     find_title = r"(?<=Title: ).*"
     find_auth = r"(?<=Author: ).*"
+    find_lang = r"(?<=Language: ).*"
 
     while True:
         book_number = random.randint(11, 49987) #11 is the first non janky book, 49987 is the last
@@ -22,12 +23,12 @@ def find_good_book():
         title_match = re.search(find_title, text) #regex to find title, author, and header
         author_match = re.search(find_auth, text)
         header_match = re.search(find_header, text)
+        lang = re.search(find_lang, text)
 
-        if(title_match and author_match and header_match): #if everything looks kosher return the book
-            return text[(header_match.span()[1]):],title_match.group(),author_match.group(),book_number
+        if(title_match and author_match and header_match and lang): #if everything looks kosher return the book
+            if lang.group() == "English":
+                return text[(header_match.span()[1]):],title_match.group(),author_match.group(),book_number
 
 def get_book(num):
     txt = os.popen(f"curl https://www.gutenberg.org/cache/epub/{num}/pg{num}.txt").read() #curl to download plain text book
     return txt
-
-run()
---
+++ scrape_book.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+from random_book import run 
+from io import StringIO
+from html.parser import HTMLParser
+import os
+
+def scrape_and_format():
+    text, title, author, book_num = run()
+    subjects = get_subject_list(book_num)
+
+    save_file = open(f"./books/{book_num}.txt",'w+')
+    save_file.write(text)
+    save_file.close() #data point has refers to text file by path
+
+    data_point = {'text' : f"./books/{book_num}", 'subjects' : subjects, 'title' : title, 'author' : author}
+    return book_num, data_point #scrape and format will return tuple of dictionary and book id
+
+class MLStripper(HTMLParser): #class to strip html tags from curl request
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.text = StringIO()
+    def handle_data(self, d):
+        self.text.write(d)
+    def get_data(self):
+        return self.text.getvalue()
+
+def strip_tags(html): #utilizes MLStripper to clean up curl request
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+def clean_page(txt): #format data neatly to scrape from
+    stripped = strip_tags(txt)
+    list = stripped.split('\n')
+    while "" in list:
+        list.remove("")
+    return list
+
+def data_curl(num): #get web page to scrape from
+    web_pg = os.popen(f"curl https://www.gutenberg.org/ebooks/{num}").read()
+    return clean_page(web_pg)
+
+def scrape_subjects(stripped): #tie it all together and return subjects
+    subjects = []
+    for i in range(len(stripped)):
+        if stripped[i].lower() == 'subject':
+            subjects.append(stripped[i+1].lower())
+
+    #split up genre from sub-genres (denoted by -- on proj. gutenberg)
+    joined = "--".join(subjects)
+    subjects = joined.split('--')
+
+    make_set = set(subjects) #make into a set to prevent dupes
+    return list(make_set) #back into list to make it easier to work with
+
+def get_subject_list(book_num):
+    return scrape_subjects(clean_page(data_curl(book_num))) #quick one line method composition
+
+#def test():
+#    txt = os.popen(f"curl https://www.gutenberg.org/ebooks/2701").read() #curl to download plain text book
+#    clean = clean_page(txt)
